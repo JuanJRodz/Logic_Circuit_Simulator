17e4ccd5f04b3abf9e6566b59d0e6d35a25b99da
Juan J. Rodríguez León <juan.rodriguez82@upr.edu>
2022-07-26 16:34:43 -0400

    Mux Component created

--- src/Tools/Components/MUX.py
+++ src/Tools/Components/MUX.py
@@ -3,6 +3,30 @@ class MUX:
         
         self.name = name 
         
-    def function(self, *args, a, b ):
+    def function(self, inputs, s1, s0):
         
-        
+        while len(inputs) < 4: 
+            args = args + [0] 
+
+        if not s0 == 0:
+            s0 = 1
+        if not s1 == 0:
+            s1 = 1
+            
+        if s0 == 0:
+            if s1 == 0:
+                return inputs[0]
+            else:
+                return inputs[2]
+        else: 
+            if s1 == 0:
+                return inputs[1]
+            else:
+                return inputs[3]
+
+a = MUX("MUX1")
+
+w=a.function([1,2,3,4],5,0)
+print(w)
+
+            
