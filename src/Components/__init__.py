096e7401d94142601f0df1d1377d84fe9b1edb1f
Juan J. Rodríguez León <juan.rodriguez82@upr.edu>
2022-08-02 14:45:07 -0400

    Deleted Tools folder

---
+++ src/Components/__init__.py
@@ -0,0 +1,30 @@
+
+
+__all__ = ["AND",
+           "Clk",
+           "CONST",
+           "INVERTER",
+           "NAND",
+           "NOR",
+           "OR",
+           "USR",
+           "XOR"]
+
+from .AND import AND 
+from .Clock import Clk
+from .Constant import CONST
+from .Inverter import INVERTER 
+from .NAND import NAND
+from .NOR import NOR
+from .OR import OR
+from .USR import USR
+from .XOR import XOR
+
+#Here you will introduce the logic circuit components you want to use
+
+components = ["AND", "OR"]
+Inputs 
+
+for i in components 
+    if i == "AND"
+        AND
