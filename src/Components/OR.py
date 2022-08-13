792a79164060ba5de1674cce7452d5c91ad718c8
particlematter16 <anthony.poyet@upr.edu>
2022-08-12 22:52:29 -0400

    OR Gate more simplified and no return

--- src/Components/OR.py
+++ src/Components/OR.py
@@ -1,24 +1,25 @@
 class OR:
-    def __init__(self, name):
+    def __init__(self, name, output):
         
         self.name = name 
+        self.output = 0
         
     def function(self, *args):
-        one = 0
-        list_inputs = list(args)
-        num_inputs = len(list_inputs)
-        for i in range(num_inputs):
-            if list_inputs[i] >= 1: 
-                list_inputs[i] = 1
-                one = one + 1
-
-        if one == 0:
-            return 0
+        list_inputs = x + y + z + zz
+        list_value = sum(list_inputs)
+        if list_value == 0:
+            self.output = 0 
         else:
-            return 1     
+            self.output = 1
+        
                 
+a = [0,0,0]
+b = [0]
+z = [0]
+
+c = a + b + z
 
 test = OR("OR1")
 
-w=test.function(0,0,0)
+w=test.function(c, z)
 print(w)
